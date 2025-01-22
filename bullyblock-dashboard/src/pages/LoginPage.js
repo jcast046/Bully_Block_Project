65854aca1138d2db17076b048e28e06c7ad64ead
JayBraley <112284239+JayBraley@users.noreply.github.com>
2025-01-22 10:35:37 -0500

    Frontend component initialization

---
+++ bullyblock-dashboard/src/pages/LoginPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <label>
+                    Username:
+                    <input type = "text" />
+                </label>
+                <label>
+                    Password:
+                    <input type = "password" />
+                </label>
+                <button type = "submit">Login</button>
+            </form>
+        </div>
+    );
+};
