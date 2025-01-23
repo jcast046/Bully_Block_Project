cfd99ecff495632834a14a0b8bbf978087c69bdc
JayBraley <112284239+JayBraley@users.noreply.github.com>
2025-01-23 14:30:41 -0500

    Navbar iteration 1

--- bullyblock-dashboard/src/components/common/Navbar.js
+++ bullyblock-dashboard/src/components/common/Navbar.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import './Navbar.css'
+import logo from '../../assets/images/logo3-white.svg';
 
 const Navbar = () => {
     return (
         <nav>
-            <ul>
-                <li>Home</li>
-                <li>Incidents</li>
-                <li>Analytics</li>
-            </ul>
+            <div className="nav-logo">
+                <img src={logo} alt="BullyBlock Logo"/>
+            </div>
+            <div className="nav-links">
+                <a href="../../pages/Dashboard">Home</a>
+                <a href="../../pages/Incidents">Incidents</a>
+                <a href="../../pages/Login">Analytics</a>
+            </div>
         </nav>
     );
 };
