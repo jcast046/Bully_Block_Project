9c99d80d5df88a86e8b86540292f80bb43111ec2
JayBraley <112284239+JayBraley@users.noreply.github.com>
2025-01-23 20:21:31 -0500

    Navbar menu-toggle update

--- bullyblock-dashboard/src/components/common/Navbar.js
+++ bullyblock-dashboard/src/components/common/Navbar.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css'
 import logo from '../../assets/images/logo3-white.svg';
 
 const Navbar = () => {
+    // State to track whether the menu is open or closed
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Toggles the menu state between open and closed
+    const handleMenuToggle = () => {
+        setIsMenuOpen((prev) => !prev);
+    };
+    
+    // Dynamically sets the class name depending on the menu state
+    const menuToggleClass = `menu-toggle ${isMenuOpen ? 'open' : ''}`;
+    const navLinksClass = `nav-links ${isMenuOpen ? 'open' : 'close'}`;
+
     return (
         <nav>
-            <div className="nav-logo">
-                <img src={logo} alt="BullyBlock Logo"/>
+            <div className="nav-header">
+                <div className="nav-logo">
+                    <img src={logo} alt="BullyBlock Logo"/>
+                </div>
+                <div className={menuToggleClass} onClick={handleMenuToggle}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
-            <div className="nav-links">
+            <div className={navLinksClass}>
                 <a href="../../pages/Dashboard">Home</a>
                 <a href="../../pages/Incidents">Incidents</a>
                 <a href="../../pages/Login">Analytics</a>
             </div>
         </nav>
+        
     );
 };
 
