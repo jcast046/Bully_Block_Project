825e4510051d80095f3abf4c633d35074c5d7c20
Nomar Rodriguez <151795666+Nrodr015@users.noreply.github.com>
2025-01-27 22:57:59 -0500

    Updated userRoutes.js
    Fixed value from School to User

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { getUsers, getUser, addUser, updateUser, deleteUser} = require('../controllers/userController');
+const { getUsers, getUser, addUser, updateUser, deleteUser } = require('../controllers/userController');
 const getResource = require('../middleware/getResource');
-const School = require('../models/User');
+const User = require('../models/User'); 
 
 const router = express.Router();
 
@@ -9,13 +9,13 @@ const router = express.Router();
 router.get('/', getUsers);
 
 // Get a single user by ID 
-router.get('/:id', getResource(School), getUser);
+router.get('/:id', getResource(User), getUser);
 
 // Add a new user
 router.post('/', addUser);
 
 // Update a user
-router.put('/:id', getResource(School), updateUser);
+router.put('/:id', getResource(User), updateUser);
 
 // Delete a user
 router.delete('/:id', deleteUser);
